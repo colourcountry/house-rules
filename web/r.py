ec8dfd92b34566b0d90436a2b293cfe2d5ff1444
David Linton <lzta@colourcountry.net>
2011-10-07 23:21:53 +0100

    refactored to use classes rule/rulespec allow keyword refs to be overridden

--- web/r.py
+++ web/r.py
@@ -6,31 +6,23 @@ sys.path.append(os.path.dirname(__file__))
 import inlines
 import rules
 
+
 def application(environ, start_response):
     status = '200 OK'
 
-    piece = rules.Piece("black","circle")
+    myrules = rules.RuleSpec("Caravaneers",date="2009",author="Andrew Perkis")
 
-    keywords = {"move":"defs/caravaneers/move",
-                "caravan":"defs/caravaneers/caravan",
-                "mountain space":"defs/caravaneers/marked-space",
-                "victory point":"defs/common/victory-point"}
+    myrules.add("move", "defs/caravaneers/move")
+    myrules.add("caravan","defs/caravaneers/caravan")
+    myrules.add("mountain space","defs/caravaneers/marked-space")
 
-
-    defs = '<dl>'
-
-    for keyword in sorted(keywords.keys()):
-        defs += '<dt><a name="keyword-%s"></a><span class="keyword">%s</span></dt>' % (keywords[keyword], keyword)
-        defs += '<dd>%s</dd>' % rules.getHtml(keywords[keyword],piece,keywords)
-
-    defs += '</dl>'
 
     output = '''<html  xmlns="http://www.w3.org/1999/xhtml"
                      xmlns:svg="http://www.w3.org/2000/svg"
                      xmlns:xlink="http://www.w3.org/1999/xlink"
 >
     <head>
-        <title>Hello</title>
+        <title>%s</title>
         <style type="text/css">
 .svg { display: inline-block; height: 20pt; width: 20pt; vertical-align: middle }
 .keyword { text-transform: uppercase; font-weight: bold; color: blue }
@@ -38,11 +30,11 @@ def application(environ, start_response):
         </style>
     </head>
     <body>
-        <h1>hello</h1>
+        <h1>%s</h1>
         %s
     </body>
 </html>
-''' % defs
+''' % (myrules.name, myrules.name, myrules.html())
 
     response_headers = [('Content-type', 'application/xhtml+xml'),
                         ('Content-length', str(len(output)))]
--- web/rules.py
+++ web/rules.py
@@ -4,6 +4,7 @@ from Cheetah.Template import Template
 import inlines
 import urlparse
 
+
 class Piece:
     def __init__(self, colour, shape):
         self.colour = colour
@@ -12,25 +13,63 @@ class Piece:
     def html(self):
         return inlines.piece(self.colour,self.shape)
 
+DEFAULT_PIECE = Piece("black","circle")
 
-def getHtml(name,piece,keywords):
+class Rule:
+    def __init__(self, definition, piece=DEFAULT_PIECE, otherPieces=None):
+        self.definition = definition
+        self.template = Template(file="/var/www/rules/templates/"+definition)
+        self.piece = piece
+        self.otherPieces = otherPieces
+        self.keywordMap = None # don't know this yet
 
-    # reverse dict as now we want to get local names for ids
-    keywords = {a:b for (b,a) in keywords.items()}    
+    def findKeyword(self,keyword):
+        theId = urlparse.urljoin(self.definition,keyword)
+        if theId in self.keywordMap:
+            return (theId, "keyword", self.keywordMap[theId])
+        else:
+            return ("unknown", "keyword-unknown", theId)
 
-    def getKeywordsHtmlFn(keywordsDict):
-        def findId(keyword):
-            if keyword in keywordsDict:
-                return (keyword, "keyword", keywordsDict[keyword])
-            else:
-                theId = urlparse.urljoin(name,keyword)
-                if theId in keywordsDict:
-                    return (theId, "keyword", keywordsDict[theId])
-                else:
-                    return ("unknown", "keyword-unknown", theId)
-        return lambda x: '<a href="#keyword-%s" class="%s">%s</a>' % findId(x)
+    def html(self, keywordMap):
+        self.keywordMap = keywordMap
 
-    t = Template(file="/var/www/rules/templates/"+name)
-    t.piece = [piece.html()]
-    t.keywords = getKeywordsHtmlFn(keywords)
-    return str(t)
+        if self.piece:
+            self.template.piece = self.piece.html()
+        if self.otherPieces:
+            self.template.pieces = [piece.html() for piece in self.otherPieces]
+
+        self.template.keyword = lambda keyword: '<a href="#keyword-%s" class="%s">%s</a>' % self.findKeyword(keyword)
+
+        return str(self.template)
+
+class RuleSpec:
+    def __init__(self, name, **metadata):
+        # Map of local-name --> rule (file)name
+        self.definitions = {}
+
+        # For each local-name, optional map of rule (file)name --> local-name
+        # If not found, use reverse of above
+        self.keywordMaps = {}
+
+        self.rules = {}
+
+        self.name = name
+        self.metadata = metadata
+
+    def add(self,localName, definition, piece=DEFAULT_PIECE, otherPieces=None, keywords=None):
+        self.definitions[localName] = definition
+        self.keywordMaps[localName] = keywords
+        self.rules[localName] = Rule(definition, piece, otherPieces)
+
+    def html(self):
+        result = ["<dl>"]
+        for localName in self.rules:
+            keywordMap = {a:b for (b,a) in self.definitions.items()}
+            if self.keywordMaps[localName]:
+                keywordMap.update( self.keywordMaps[localName] )
+
+            result.extend([ '<dt><a name="keyword-'+self.definitions[localName]+'"></a><span class="keyword">',localName,"</span></dt>",
+                            "<dd>",self.rules[localName].html(keywordMap),"</dd>" ])
+        result.append("</dl>")
+
+        return "\n".join(result)
